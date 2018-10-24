62fed6794f1167f9772e1772ace9df7c8af6b842
MiguelAlvarez97 <miguel9730@hotmail.com>
2018-10-24 11:00:31 -0500

    clase 6

--- 01-Javascript/05-callbacks.js
+++ 01-Javascript/05-callbacks.js
@@ -4,11 +4,13 @@ const nombreDelArchivo = '05-texto.txt';
 
 
 console.log('Inicio');
-fs.readFile('04-Operadores.js',
+fs.readFile(nombreDelArchivo,
     'utf-8',
     (error, contenidoArchivo) => { //CallBack
         if(error){
             console.error(error);
+
+
             try{
                 throw new Error(error); //hace terminar abruptamente, en try catch continua
             } catch (e){
@@ -17,8 +19,13 @@ fs.readFile('04-Operadores.js',
 
             console.log('extra')
         } else{
+            fs.writeFile(nombreDelArchivo, contenidoArchivo + contenidoAAgregar, (err)=>{
+                if(err) throw err;
+                console.log('Archivo completado!');
+            });
             console.log('Si sirvio  ', contenidoArchivo)
 
         }
     });
+
 console.log('FIN')
