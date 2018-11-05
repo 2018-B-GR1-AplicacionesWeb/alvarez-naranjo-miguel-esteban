466d5f1f87f3565fbd018b94a49ec8efbf41ef30
MiguelAlvarez97 <miguel9730@hotmail.com>
2018-11-05 10:57:16 -0500

    observables

--- 02-typescript/02-observables.js
+++ 02-typescript/02-observables.js
@@ -1,13 +1,66 @@
-//declare var module:any
+// 02-observables.ts
+// import { PaqueteUno, Paquete dos} from 'rxjs';
+// import * as rxjs from 'rxjs';
+// import {Observable} from "rxjs";
+// declare var module:any;
 const rxjs = require('rxjs');
-const observableUno$ = rxjs.of(1, 2, 3, 4, 5);
-console.log(observableUno$);
-//import {Observable} from "rxjs";
-//const observableUno$: Observable<number> = rxjs.of(1);
+const map = require('rxjs/operators').map;
+const disctinct = require('rxjs/operators').distinct;
+const observableUno$ = rxjs.of([1, 2, 3], 3, 'Hola', 3, true, 3, { nombre: 'Adrian' }, new Date(), 3);
 console.log(observableUno$);
 observableUno$
+    .pipe(disctinct(), map((valor) => {
+    console.log('Valor', valor);
+    return {
+        data: valor
+    };
+}))
+    .pipe()
+    .pipe()
     .subscribe((ok) => {
-    console.log(ok);
+    console.log('En ok', ok);
 }, (error) => {
     console.log(error);
+}, () => {
+    console.log('Completado');
 });
+const promesita = () => {
+    // @ts-ignore
+    return new Promise((resolve, reject) => {
+        reject(':(');
+    });
+};
+/*
+const observableDePromesa$ = rxjs.from(promesita());
+
+observableDePromesa$
+    .pipe(
+        map(
+            (valor) => {
+                return {
+                    data: valor
+                }
+            }
+        )
+    )
+    .subscribe(
+        (objetoFeliz) => {
+            console.log(objetoFeliz);
+        },
+        (error) => {
+            console.log(error);
+        }
+    );*/
+async function ejecutarCodigoSyncrono() {
+    console.log('INICIO');
+    try {
+        const resultadoPromesita = await promesita();
+        console.log(resultadoPromesita);
+    }
+    catch (e) {
+        console.log('Error en promesita', e);
+    }
+    console.log('FIN');
+}
+ejecutarCodigoSyncrono();
+console.log("*************************************************************************************");
--- 02-typescript/02-observables.ts
+++ 02-typescript/02-observables.ts
@@ -1,21 +1,105 @@
 // 02-observables.ts
-//import{Paqueteuno, PaqueteDos} from 'rxjs';
-//import * as rxjs from 'rxjs';
-declare var require: any;
-//declare var module:any
-const rxjs = require('rxjs');
-const observableUno$ = rxjs.of(1,2,3,4,5);
-console.log(observableUno$);
-//import {Observable} from "rxjs";
+// import { PaqueteUno, Paquete dos} from 'rxjs';
+// import * as rxjs from 'rxjs';
+// import {Observable} from "rxjs";
 
-//const observableUno$: Observable<number> = rxjs.of(1);
+declare var require: any;
+// declare var module:any;
+
+const rxjs = require('rxjs');
+const map = require('rxjs/operators').map;
+const disctinct = require('rxjs/operators').distinct;
+
+const observableUno$ = rxjs.of(
+    [1, 2, 3],
+    3,
+    'Hola',
+    3,
+    true,
+    3,
+    {nombre: 'Adrian'},
+    new Date(),
+    3,
+);
+
 console.log(observableUno$);
+
 observableUno$
+    .pipe(
+        disctinct(),
+        map(
+            (valor) => {
+                console.log('Valor', valor);
+                return {
+                    data: valor
+                };
+            }
+        )
+    )
+    .pipe(
+
+    )
+    .pipe(
+
+    )
     .subscribe(
-         (ok) =>{
-            console.log(ok);
-},
-    (error) =>{
+        (ok) => {
+            console.log('En ok', ok);
+        },
+        (error) => {
             console.log(error);
-}
+        },
+        () => {
+            console.log('Completado');
+        }
     );
+
+
+const promesita = () => {
+    // @ts-ignore
+    return new Promise(
+        (resolve, reject) => {
+            reject(':(');
+        }
+    )
+};
+/*
+const observableDePromesa$ = rxjs.from(promesita());
+
+observableDePromesa$
+    .pipe(
+        map(
+            (valor) => {
+                return {
+                    data: valor
+                }
+            }
+        )
+    )
+    .subscribe(
+        (objetoFeliz) => {
+            console.log(objetoFeliz);
+        },
+        (error) => {
+            console.log(error);
+        }
+    );*/
+
+async function ejecutarCodigoSyncrono(){
+    console.log('INICIO');
+
+    try{
+        const resultadoPromesita = await promesita();
+        console.log(resultadoPromesita);
+
+    }catch (e){
+        console.log('Error en promesita', e);
+
+    }
+
+
+
+    console.log('FIN');
+}
+ejecutarCodigoSyncrono();
+console.log("*************************************************************************************");
