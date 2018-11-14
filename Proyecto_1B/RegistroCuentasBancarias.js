fb60661a99c8a2e11b7f89fb1880a6bbd84a0845
MiguelAlvarez97 <miguel9730@hotmail.com>
2018-11-14 03:56:27 -0500

    proyecto 1b

--- Proyecto_1B/RegistroCuentasBancarias.js
+++ Proyecto_1B/RegistroCuentasBancarias.js
@@ -2,8 +2,11 @@
 exports.__esModule = true;
 var rxjs = require('rxjs');
 var inquirer = require('inquirer');
+var fs = require('fs');
 var OpcionesFormulario_1 = require("./OpcionesFormulario");
-console.log('***BIENVENIDO AL REGISTRO BANCARIO***\n');
+console.log('---------------------------------------------------\n');
+console.log('****BIENVENIDO AL REGISTRO DE CUENTAS BANCARIAS****\n');
+console.log('---------------------------------------------------\n');
 inquirer
     .prompt(OpcionesFormulario_1.opciones)
     .then(function (answers) {
@@ -11,10 +14,58 @@ inquirer
         inquirer
             .prompt(OpcionesFormulario_1.crearCuentaFormulario)
             .then(function (answersFormulario) {
+            console.log(JSON.stringify(answersFormulario, null, ''));
+            console.log('********************');
             console.log(answersFormulario);
+            //guardarEnArchivo('jsonUsuarios.json', JSON.stringify(answersFormulario, null, ''))
+            nuevaPromesaLectura
+                .then(function (contenidoArchivo) {
+                console.log('Todo bien', contenidoArchivo);
+                return nuevaPromesaEscritura(contenidoArchivo, JSON.stringify(answersFormulario, null, ''));
+            })
+                .then(function (contenidoCompleto) {
+                console.log('Contenido completo', contenidoCompleto);
+            })["catch"](function (resultadoError) {
+                console.log('Algo malo paso', resultadoError);
+            });
         });
     }
     if (answers.opcionesPrincipales === 'Salir') {
         console.log('HASTA LUEGO! :) ');
     }
 });
+var guardarEnArchivo = function (nombreDelArchivo, datos) {
+    fs.writeFile(nombreDelArchivo, datos, function (error) {
+        return new Promise(function (resolve, reject) {
+            if (error) {
+                reject(error);
+            }
+            else {
+                resolve('USUARIO CREADO');
+            }
+        });
+    });
+};
+var nuevaPromesaLectura = new Promise(function (resolve) {
+    fs.readFile('jsonUsuarios.json', 'utf-8', function (err, contenidoArchivo) {
+        if (err) {
+            resolve('');
+        }
+        else {
+            resolve(contenidoArchivo);
+        }
+    });
+});
+var nuevaPromesaEscritura = function (contenidoLeido, datos) {
+    return new Promise(function (resolve, reject) {
+        var contenido = contenidoLeido ? contenidoLeido + datos : datos;
+        fs.writeFile('jsonUsuarios.json', contenido, function (err) {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(contenido);
+            }
+        });
+    });
+};
--- Proyecto_1B/RegistroCuentasBancarias.ts
+++ Proyecto_1B/RegistroCuentasBancarias.ts
@@ -1,7 +1,11 @@
 const rxjs = require('rxjs');
 const inquirer = require('inquirer');
+const fs = require('fs');
+declare var Promise:any;
 import {opciones, crearCuentaFormulario} from './OpcionesFormulario'
-console.log('***BIENVENIDO AL REGISTRO BANCARIO***\n')
+console.log('---------------------------------------------------\n');
+console.log('****BIENVENIDO AL REGISTRO DE CUENTAS BANCARIAS****\n');
+console.log('---------------------------------------------------\n');
 
 inquirer
     .prompt(opciones)
@@ -10,7 +14,27 @@ inquirer
             inquirer
                 .prompt(crearCuentaFormulario)
                 .then(answersFormulario =>{
+                    console.log(JSON.stringify(answersFormulario, null, ''));
+                    console.log('********************');
                     console.log(answersFormulario);
+                    //guardarEnArchivo('jsonUsuarios.json', JSON.stringify(answersFormulario, null, ''))
+                    nuevaPromesaLectura
+                        .then(
+                            (contenidoArchivo) => {
+                                console.log('Todo bien', contenidoArchivo);
+                                return nuevaPromesaEscritura(contenidoArchivo, JSON.stringify(answersFormulario, null, ''))
+                            }
+                        )
+                        .then(
+                            (contenidoCompleto) => {
+                                console.log('Contenido completo', contenidoCompleto);
+                            }
+                        )
+                        .catch(
+                            (resultadoError) => {
+                                console.log('Algo malo paso', resultadoError);
+                            }
+                        );
                 });
         }
         if(answers.opcionesPrincipales === 'Salir'){
@@ -18,3 +42,53 @@ inquirer
         }
 
     });
+
+
+
+const guardarEnArchivo = (nombreDelArchivo, datos) =>{
+    fs.writeFile(nombreDelArchivo,datos,
+        (error)=> {
+            return new Promise(
+                (resolve,reject)=>{
+                    if(error){
+                        reject(error)
+                    }else {
+                        resolve ('USUARIO CREADO')
+                    }
+                }
+            )
+        });
+}
+
+const nuevaPromesaLectura = new Promise(
+    (resolve) => {
+        fs.readFile('jsonUsuarios.json', 'utf-8',
+            (err, contenidoArchivo) => {
+                if (err) {
+                    resolve('');
+                } else {
+                    resolve(contenidoArchivo);
+                }
+            });
+    }
+);
+
+
+const nuevaPromesaEscritura = (contenidoLeido, datos) => {
+    return new Promise(
+        (resolve, reject) => {
+
+            const contenido = contenidoLeido ? contenidoLeido + datos : datos;
+
+            fs.writeFile('jsonUsuarios.json', contenido,
+                (err,) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(contenido);
+                    }
+                });
+        }
+    );
+};
+
