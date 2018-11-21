9110e902538f6f16bd78f5380e9968493815503a
MiguelAlvarez97 <miguel9730@hotmail.com>
2018-11-21 10:55:01 -0500

    cookies

--- 03-http/video/src/main.ts
+++ 03-http/video/src/main.ts
@@ -1,10 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
-import {} from 'http-server' //en typescript para importar
+// const http_server = require('http-server'); // JS
+import * as cookieParser from 'cookie-parser';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
+    await app.listen(3000);
 }
+
 bootstrap();
